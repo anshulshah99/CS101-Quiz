9ef793e98f4a08d56d5cfb4a3a65ff4418b7684f
anshulshah99 <as817@duke.edu>
2020-02-27 11:40:20 -0500

    finished while_true() function in while_loops.py

--- while_loops.py
+++ while_loops.py
@@ -23,7 +23,7 @@ while i < len(lst):
 #How to get out of a while loop
 
 def while_true():
-    stop = ["continue", "break", "break", "return", "return", "print", "print"]
+    stop = ["continue", "break", "return", "print"]
     for s in stop:
         hit_cond = random.choice([0, 1, 1])
         if s == "continue":
@@ -34,43 +34,76 @@ def while_true():
                 check = i_incr * random.randint(10, 25) + 1
             if hit_cond:
                 check = i_incr * random.randint(10, 25)
-            question = f"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\ti += {i_incr}\n\tif i == {check}:\n\t\tcontinue"
-            correct_ans = "The code above creates an infinite loop"
+            question = rf"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\ti += {i_incr}\n\tif i == {check}:\n\t\tcontinue"
+            correct = "The code above creates an infinite loop"
             wrong1 = check//i_incr
             wrong2 = check
             if wrong2 == wrong1:
                 wrong2 = check - 1
             wrong3 = "Error: Invalid stopping condition"
-            print(question)
+            print(question, correct, wrong1, wrong2, wrong3)
         if s == "break":
-            position = random.choice(["before", "after"])
+            #position = random.choice(["before", "after"])
             i_incr = random.choice([1, 2, 3])
             if i_incr == 1:
                 check = random.randint(1, 50) * -1
             if i_incr in (2, 3):
                 check = i_incr * random.randint(10, 25) + 1
-            if hit_cond == 0 and position == "before":
-                question = f"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\tif i == {check}:\n\t\tbreak\n\ti += {i_incr}"
-                correct_ans = "The code above creates an infinite loop"
+            if hit_cond == 0:
+                question = rf"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\ti += {i_incr}\n\tif i == {check}:\n\t\tbreak"
+                correct = "The code above creates an infinite loop"
                 wrong1 = check // i_incr
                 wrong2 = check + i_incr
                 wrong3 = check
                 if wrong3 == wrong1:
                     wrong3 = check - i_incr
-            if hit_cond == 0 and position == "after":
-                question = f"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\ti += {i_incr}\n\tif i == {check}:\n\t\tbreak"
-                correct_ans = "The code above creates an infinite loop"
-                wrong1 = check // i_incr
-                wrong2 = check + i_incr
-                wrong3 = check
-                if wrong3 == wrong1:
-                    wrong3 = check - i_incr
-
-            if hit_cond:
+            if hit_cond == 1:
                 check = i_incr * random.randint(10, 25)
-            if hit_cond == 1 and position == "before":
-                question = f"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\tif i == {check}:\n\t\tbreak\n\ti += {i_incr}"
+            if hit_cond == 1:
+                question = rf"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\tif i == {check}:\n\t\tbreak\n\ti += {i_incr}"
+                correct = check
+                wrong1 = check - i_incr
+                if wrong1 == correct:
+                    wrong1 -= 1
+                wrong2 = "The code above creates an infinite loop"
+                wrong3 = check + i_incr
+            print(question, correct, wrong1, wrong2, wrong3)
+        if s == "return":
+            i_incr = random.choice([1, 2, 3])
+            if i_incr == 1:
+                check = random.randint(1, 50) * -1
+            if i_incr in (2, 3):
+                check = i_incr * random.randint(10, 25) + 1
+            if hit_cond == 0:
+                question = rf"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\ti += {i_incr}\n\tif i == {check}:\n\t\treturn i"
+                correct = "The code above creates an infinite loop"
+                wrong1 = check // i_incr
+                wrong2 = check + i_incr
+                wrong3 = check
+                if wrong3 == wrong1:
+                    wrong3 = check - i_incr
+            if hit_cond == 1:
+                check = i_incr * random.randint(10, 25)
+            if hit_cond == 1:
+                question = rf"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\tif i == {check}:\n\t\treturn i\n\ti += {i_incr}"
+                correct = check
+                wrong1 = check - i_incr
+                if wrong1 == correct:
+                    wrong1 -= 1
+                wrong2 = "The code above creates an infinite loop"
+                wrong3 = check + i_incr
+            print(question, correct, wrong1, wrong2, wrong3)
+        if s == "print":
+            i_incr = random.choice([1, 2, 3])
+            check = i_incr * random.randint(10, 25)
+            question = rf"What will the value of i be when the code below is done executing?\ni = 0\nwhile True:\n\tif i == {check}:\n\t\tprint(i)\n\ti += {i_incr}"
+            correct = "The code above creates an infinite loop"
+            wrong1 = check
+            wrong2 = check + i_incr
+            wrong3 = "Error: Invalid print statement"
+            print(question, correct, wrong1, wrong2, wrong3)
     return question
 
+
 if __name__ == '__main__':
-    print(while_true())
+    while_true()
